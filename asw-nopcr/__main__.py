4c48356011c08b6ead63a2a2bb0babac4c1d81c3
Tom Harrop <twharrop@gmail.com>
2017-06-22 12:53:00 +1200

    pipeline ready, will need to test steps

--- asw-nopcr/__main__.py
+++ asw-nopcr/__main__.py
@@ -17,6 +17,30 @@ import os
 import shutil
 
 
+##################
+# task functions #
+##################
+
+# test function for checking input/output passed to job_script and parsing
+# by src/sh/io_parser
+test_job_function = tompltools.generate_job_function(
+    job_script='src/sh/io_parser',
+    job_name='test',
+    verbose=True)
+
+
+# concatenate function for merging fastq.gz files in ruffus
+def concatenate_readfiles(input_files, output_file):
+    buffer_size = 8192
+    with open(output_file, 'wb') as f:
+        for file_path in input_files:
+            with open(file_path, 'rb') as open_file:
+                chunk = True
+                while chunk:
+                    chunk = open_file.read(buffer_size)
+                    f.write(chunk)
+
+
 ############
 # Pipeline #
 ############
@@ -25,13 +49,6 @@ def main():
     parser = ruffus.cmdline.get_argparse(
         description='ASW PCR-free assembly pipeline.')
     options = parser.parse_args()
-
-    # test function for checking input/output passed to job_script and parsing
-    # by src/sh/io_parser
-    test_job_function = tompltools.generate_job_function(
-        job_script='src/sh/io_parser',
-        job_name='test',
-        verbose=True)
 
     # initialise pipeline
     main_pipeline = ruffus.Pipeline.pipelines['main']
@@ -53,20 +70,25 @@ def main():
     # trim and decontaminate PE file
     trimmed_reads = main_pipeline.subdivide(
         name='bbduk',
-        # task_func=tompltools.generate_job_function(
-        #     job_script='src/sh/bbduk',
-        #     job_name='bbduk',
-        #     ntasks=1,
-        #     cpus_per_task=8,
-        #     mem_per_cpu=6800),
-        task_func=test_job_function,
+        task_func=tompltools.generate_job_function(
+            job_script='src/sh/bbduk',
+            job_name='bbduk',
+            ntasks=1,
+            cpus_per_task=8,
+            mem_per_cpu=6800),
         input=[[raw_pe100_files], [raw_pe150_files]],
         filter=ruffus.formatter(r'.+/pe(?P<PE>\d+)/.+.fastq.gz'),
         output=[r'output/bbduk/pe{PE[0]}_filtered_trimmed.fastq.gz'])
 
+    # merge read files for combined QC steps (histograms, etc)
+    combined_pe_reads = main_pipeline.merge(
+        name='combined_pe_reads',
+        task_func=concatenate_readfiles,
+        input=trimmed_reads,
+        output='output/bbduk/pe_merged.fastq.gz')
+
     # merge overlapping PE reads
     # the 100b PE reads don't merge, so exclude them
-    # need to modify trimming to take into account 2 PE libs
     main_pipeline.transform(
         name='bbmerge',
         task_func=test_job_function,
@@ -83,7 +105,7 @@ def main():
             job_name='kmergenie',
             ntasks=1,
             cpus_per_task=8),
-        input=trimmed_reads,
+        input=combined_pe_reads,
         filter=ruffus.formatter(),
         output='output/kmergenie/histogram_report.html')
 
@@ -96,7 +118,7 @@ def main():
             ntasks=1,
             cpus_per_task=8,
             mem_per_cpu=6800),
-        input=trimmed_reads,
+        input=combined_pe_reads,
         filter=ruffus.formatter(),
         output='output/bbduk/uniqueness_histogram.txt')
 
@@ -115,7 +137,7 @@ def main():
         task_func=tompltools.generate_job_function(
             job_script='src/r/plot_quality_histogram.R',
             job_name='plot_quality_histogram'),
-        input=trimmed_reads,
+        input=combined_pe_reads,
         filter=ruffus.formatter(),
         output='output/bbduk/quality_histogram_plot.pdf')
 
@@ -128,7 +150,7 @@ def main():
             ntasks=1,
             cpus_per_task=8,
             mem_per_cpu=6800),
-        input=trimmed_reads,
+        input=combined_pe_reads,
         output='output/bbnorm/pe_merged_normalised.fastq.gz')
 
     # kmer plots
@@ -143,39 +165,20 @@ def main():
 
     # meraculous assemblies
     kmer_lengths = ['31', '41', '51']
-    meraculous = main_pipeline.subdivide(
+    meraculous = main_pipeline.collate(
         name='meraculous',
         task_func=test_job_function,
-        input=[trimmed_reads, binned_reads],
+        input=trimmed_reads,
         filter=ruffus.formatter(),
         output=[('{subdir[0][1]}/meraculous/{subdir[0][0]}/run_' + x +
                  'mer/meraculous_final_results/final.scaffolds.fa')
-                for x in kmer_lengths])
-
-    meraculous_diploid2 = main_pipeline.subdivide(
-        name='meraculous_diploid2',
-        task_func=test_job_function,
-        input=[trimmed_reads, binned_reads],
-        filter=ruffus.formatter(),
-        output=[('{subdir[0][1]}/meraculous_diploid2/{subdir[0][0]}/run_' + x +
-                 'mer/meraculous_final_results/final.scaffolds.fa')
-                for x in kmer_lengths])
-
-    # soap assembly
-    soap = main_pipeline.subdivide(
-        name='soap',
-        task_func=test_job_function,
-        input=[trimmed_reads, binned_reads],
-        filter=ruffus.formatter(),
-        output=[('{subdir[0][1]}/soap_denovo2/{subdir[0][0]}/run_' + x +
-                 'mer/assembly.scafSeq')
                 for x in kmer_lengths])
 
     # assembly statistics
     assembly_statistics = main_pipeline.merge(
         name='assembly_statistics',
         task_func=test_job_function,
-        input=[meraculous, soap, meraculous_diploid2],
+        input=meraculous,
         output='output/assembly_statistics/statistics.txt')
 
 
